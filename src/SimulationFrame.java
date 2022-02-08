a2aaa247a31ca7d291fb18705c648f7e17fa3c45
Yamen Malik <60931606+Yamen-Malik@users.noreply.github.com>
2022-02-08 16:58:29 +0200

    Added a min size for the SimulationFrame

--- src/SimulationFrame.java
+++ src/SimulationFrame.java
@@ -1,6 +1,11 @@
 import javax.swing.JFrame;
+import java.awt.Dimension;
 public class SimulationFrame extends JFrame {
-    public SimulationFrame(int width, int height) {
+    public SimulationFrame(int width, int height, Integer... minimum) {
+        int minWidth, minHeight;
+        minWidth = minimum.length == 2? minimum[0] : (int) width/2;
+        minHeight = minimum.length == 2? minimum[1] : (int) height/2;
+        setMinimumSize(new Dimension(minWidth, minHeight));
         setSize(width, height);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
